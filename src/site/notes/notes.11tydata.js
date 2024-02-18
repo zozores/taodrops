9c80ab7a4dedbdd22639f8bec33aa4d710d0aaa2
Thiago (Zozô) Ozores <tozores@gmail.com>
2024-02-17 22:01:51 -0300

    Update files

--- src/site/notes/notes.11tydata.js
+++ src/site/notes/notes.11tydata.js
@@ -17,9 +17,6 @@ module.exports = {
       }
       return data.permalink || undefined;
     },
-    lang: (data) => {
-      return data.lang || undefined;
-    },
     settings: (data) => {
       const noteSettings = {};
       allSettings.forEach((setting) => {
